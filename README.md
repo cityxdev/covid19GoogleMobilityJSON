f922301cad71f77490dff3fe3dbb8fe0cbc6a027
Sérgio Nunes <sergioasnunes@gmail.com>
2020-06-19 01:15:57 +0100

    New version that breaks dependencies
    As this dataset continues growing, we reached a file size limit of Github. This way, the output structure changed to make files smaller. This was done by moving regional data to individual files instead of being stored in the country file.

--- README.md
+++ README.md
@@ -1,12 +1,38 @@
 # covid19GoogleMobilityJSON
 A way to transform Google's mobility data during the COVID-19 pandemic to a JSON file per country
 
-Reads from the CSV available at https://www.gstatic.com/covid19/mobility/Global_Mobility_Report.csv
+<h2>New version that breaks dependencies</h2>
+<p>As this dataset continues growing, we reached a file size limit of Github. This way, the output structure changed to make files smaller. This was done by moving regional data to individual files instead of being stored in the country file.</p>
+<p>The output has now the following structure:</p>
 
-I will try to run once a day and push an update on fresh data
+    --data
+      |_____countries
+      |     |__________google_mobility_data_XX.json
+      |     |__________google_mobility_data_XY.json
+      |
+      |_____subregions1
+      |     |__________google_mobility_data_XX-AA.json
+      |     |__________google_mobility_data_XX-AB.json
+      |     |__________google_mobility_data_XY-subregion1name.json
+      |     |__________google_mobility_data_XY-subregion1name.json
+      |
+      |______subregions2                                                               (currently, only for USA)
+             |__________google_mobility_data_XX_subregion1name_subregion2name.json
+           
+- The two letter code for countries is stable and covers the whole dataset
+- The iso_3166_2 code for subregion1 is used where possible. If unavailable, name is used instead
+- There are no codes for subregion2. It is also not possible to use the iso_3166_2 code to group subregion2 because it is not provided in the source
+<hr/>
+
+<br/>
+<p>Reads from the CSV available at https://www.gstatic.com/covid19/mobility/Global_Mobility_Report.csv
+<br/>
+More info on this data: https://www.google.com/covid19/mobility/data_documentation.html?hl=en#about-this-data</p>
+
+<p>I will try to run once a day and push an update on fresh data</p>
 
 <br/><br/>
 **You can access the data using a URL like:**
 
-```https://cityxdev.github.io/covid19GoogleMobilityJSON/data/google_mobility_data_```[2 letter country code]```.json```
-Example: ```https://cityxdev.github.io/covid19GoogleMobilityJSON/data/google_mobility_data_PT.json```
+```https://cityxdev.github.io/covid19GoogleMobilityJSON/data/countries/google_mobility_data_```[2 letter country code]```.json```
+Example: ```https://cityxdev.github.io/covid19GoogleMobilityJSON/data/countries/google_mobility_data_PT.json```
--- retrieve_data.py
+++ retrieve_data.py
@@ -1,3 +1,5 @@
+# covid19GoogleMobilityJSON - v2.0 - 2020-06-19 - https://github.com/cityxdev/covid19GoogleMobilityJSON
+
 import csv
 import json
 
@@ -6,6 +8,8 @@ import urllib2
 csvdata = urllib2.urlopen("https://www.gstatic.com/covid19/mobility/Global_Mobility_Report.csv")
 
 countries = {}
+subregions1 = {}
+subregions2 = {}
 
 fieldnames = (
     "country_region_code",
@@ -23,7 +27,7 @@ fieldnames = (
     "residential_percent_change_from_baseline"
 )
 reader = csv.DictReader(csvdata, fieldnames)
-i = 0;
+i = 0
 for row in reader:
     i += 1
     if i == 1:
@@ -31,37 +35,69 @@ for row in reader:
 
     countrykey = row["country_region_code"]
     if not countries.get(countrykey):
-        countries[countrykey] = {
-            "country": [],
-            "regions": {}
-        }
+        countries[countrykey] = []
+
+    subregion1key = None
+    subregion2key = None
+    if row["sub_region_1"] != '':
+        subregion1key = row["iso_3166_2_code"] if row["iso_3166_2_code"] != '' else countrykey+"_"+row["sub_region_1"]
+        if not subregions1.get(subregion1key):
+            subregions1[subregion1key] = []
+        if row["sub_region_2"] != '':
+            subregion2key = subregion1key+"_"+row["sub_region_2"]
+            if not subregions2.get(subregion2key):
+                subregions2[subregion2key] = []
 
     values = {
         "date": row["date"],
-        "retail_and_recreation_percent_change_from_baseline": float(
-            row["retail_and_recreation_percent_change_from_baseline"]) if row[
-                                                                              "retail_and_recreation_percent_change_from_baseline"] != '' else None,
-        "grocery_and_pharmacy_percent_change_from_baseline": float(
-            row["grocery_and_pharmacy_percent_change_from_baseline"]) if row[
-                                                                             "grocery_and_pharmacy_percent_change_from_baseline"] != '' else None,
-        "parks_percent_change_from_baseline": float(row["parks_percent_change_from_baseline"]) if row[
-                                                                                                      "parks_percent_change_from_baseline"] != '' else None,
-        "transit_stations_percent_change_from_baseline": float(row["transit_stations_percent_change_from_baseline"]) if
-        row["transit_stations_percent_change_from_baseline"] != '' else None,
-        "workplaces_percent_change_from_baseline": float(row["workplaces_percent_change_from_baseline"]) if row[
-                                                                                                                "workplaces_percent_change_from_baseline"] != '' else None,
-        "residential_percent_change_from_baseline": float(row["residential_percent_change_from_baseline"]) if row[
-                                                                                                                  "residential_percent_change_from_baseline"] != '' else None
+
+        "retail_and_recreation_percent_change_from_baseline":
+            float(row["retail_and_recreation_percent_change_from_baseline"])
+            if row["retail_and_recreation_percent_change_from_baseline"] != ''
+            else None,
+
+        "grocery_and_pharmacy_percent_change_from_baseline":
+            float(row["grocery_and_pharmacy_percent_change_from_baseline"])
+            if row["grocery_and_pharmacy_percent_change_from_baseline"] != ''
+            else None,
+
+        "parks_percent_change_from_baseline":
+            float(row["parks_percent_change_from_baseline"])
+            if row["parks_percent_change_from_baseline"] != ''
+            else None,
+
+        "transit_stations_percent_change_from_baseline":
+            float(row["transit_stations_percent_change_from_baseline"])
+            if row["transit_stations_percent_change_from_baseline"] != ''
+            else None,
+
+        "workplaces_percent_change_from_baseline":
+            float(row["workplaces_percent_change_from_baseline"])
+            if row["workplaces_percent_change_from_baseline"] != ''
+            else None,
+
+        "residential_percent_change_from_baseline":
+            float(row["residential_percent_change_from_baseline"])
+            if row["residential_percent_change_from_baseline"] != ''
+            else None
     }
 
-    if row["sub_region_1"] != '':
-        regionkey = row["sub_region_1"] + (('_' + row["sub_region_2"]) if row["sub_region_2"] != '' else '')
-        if not countries[countrykey]["regions"].get(regionkey):
-            countries[countrykey]["regions"][regionkey] = []
-        countries[countrykey]["regions"][regionkey].append(values)
+    if subregion1key:
+        if subregion2key:
+            subregions2[subregion2key].append(values)
+        else:
+            subregions1[subregion1key].append(values)
     else:
-        countries[countrykey]["country"].append(values)
+        countries[countrykey].append(values)
 
 for key in countries:
-    jsonfile = open('data/google_mobility_data_' + key + '.json', 'w')
+    jsonfile = open('data/countries/google_mobility_data_' + key + '.json', 'w')
     json.dump(countries[key], jsonfile)
+
+for key in subregions1:
+    jsonfile = open('data/subregions1/google_mobility_data_' + key + '.json', 'w')
+    json.dump(subregions1[key], jsonfile)
+
+for key in subregions2:
+    jsonfile = open('data/subregions2/google_mobility_data_' + key + '.json', 'w')
+    json.dump(subregions2[key], jsonfile)
